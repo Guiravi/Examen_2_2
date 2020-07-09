237d5e9f295f65233aba8434123b8dbf2ab123db
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 22:12:46 -0600

    [CrearPizza] Se cambió el constructor de pizza para que inicialice la lista de ingredientes de comida

--- Examen_2_2/Examen_2_2/Clases_Concretas/Pizza.cs
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Pizza.cs
@@ -1,9 +1,16 @@
 ﻿using Examen_2_2.Clases_Abstractas;
+using System.Collections.Generic;
 
 namespace Examen_2_2.Clases_Concretas
 {
     public class Pizza : Comida
     {
+        public Pizza()
+        {
+            Comida comida = this;
+            comida.Ingredientes = new List<Ingrediente>();
+        }
+
         public override string GetNombre()
         {
             return "Pizza";
