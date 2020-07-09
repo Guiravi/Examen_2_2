2feead36bf9140d3a489f05cbf621a5a77ae5540
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 22:11:39 -0600

    [CrearPizza] Se creó la clase Masa pan pizza que hereda de ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/MasaPanPizza.cs
@@ -0,0 +1,17 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class MasaPanPizza : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Masa Pan Pizza";
+        }
+
+        public override double GetPrecio()
+        {
+            return 800;
+        }
+    }
+}
