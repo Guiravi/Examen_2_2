c1a9e372225c62984e2b6cb2f4122c499089160d
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:43:45 -0600

    [CrearPizza] Se crea la clase concreta Carne que deriva de ingrediente.

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Carne.cs
@@ -0,0 +1,17 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Carne : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Carne";
+        }
+
+        public override double GetPrecio()
+        {
+            return 1000;
+        }
+    }
+}
