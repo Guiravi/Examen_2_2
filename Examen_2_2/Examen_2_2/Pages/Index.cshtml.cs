3efa2625dcbd171925f6eccf846c20563a770c79
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 19:36:51 -0600

    [FormularioPizza] Se mejor la interfáz principal

--- Examen_2_2/Examen_2_2/Pages/Index.cshtml.cs
+++ Examen_2_2/Examen_2_2/Pages/Index.cshtml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Examen_2_2.Pages
 {
     public class IndexModel : PageModel
     {
-        public IActionResult OnPostEntrarFormulario()
-        {
-            return Redirect("Formulario");
-        }
-
-
         public void OnGet()
         {
 
---
+++ Examen_2_2/Examen_2_2/Pages/Recibo.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Examen_2_2.Pages
+{
+    public class ReciboModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int Precio { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string TipoComida { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
