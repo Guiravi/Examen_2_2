ee0baf7f7e3bed9263560134414ac19e6576c337
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 21:06:05 -0600

    [CrearPizza] Se crea la instancia Pedido y la lista de ingredientes a seleccionar

--- Examen_2_2/Examen_2_2/Pages/Formulario.cshtml.cs
+++ Examen_2_2/Examen_2_2/Pages/Formulario.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Examen_2_2.Clases_Abstractas;
+using Examen_2_2.Clases_Concretas;
 
 namespace Examen_2_2.Pages
 {
     public class FormularioModel : PageModel
     {
-        public List<int> listaIngredientes;
+        public List<Ingrediente> listaIngredientes;
+        public Pedido pedido;
 
         [BindProperty]
         public string Anotaciones { get; set; }
@@ -15,10 +18,20 @@ namespace Examen_2_2.Pages
         public int Masa { set; get; }
 
         [BindProperty]
-        public List<int> ListaIngredientesSelec { get; set; }
+        public List<string> ListaIngredientesSelec { get; set; }
 
         [BindProperty]
         public int Tamano { get; set; }
+
+        public FormularioModel()
+        {
+            ListaIngredientesSelec = new List<string>();
+            listaIngredientes = new List<Ingrediente>();
+            pedido = new Pedido();
+            Pizza pizza = new Pizza();
+            pedido.AddComida(pizza);
+
+        }
 
         [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult OnPost()
