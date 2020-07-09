79c4a95c5e93c6882b8b8eb7c5398c24e2996764
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:45:25 -0600

    [CrearPizza] Se crea la clase concreta Pina que deriva de Ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Pina.cs
@@ -0,0 +1,18 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Pina : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Piña";
+        }
+
+        public override double GetPrecio()
+        {
+            return 800;
+        }
+    }
+}
