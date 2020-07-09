bd577ed0f899c4184701c3a0db04980022411ee1
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 22:10:19 -0600

    [CrearPizza] Se agregó un método GetTamano en comida

--- Examen_2_2/Examen_2_2/Clases_Abstractas/Comida.cs
+++ Examen_2_2/Examen_2_2/Clases_Abstractas/Comida.cs
@@ -5,13 +5,18 @@ namespace Examen_2_2.Clases_Abstractas
     public abstract class Comida
     {
         public List<Ingrediente> Ingredientes;
-        private string tamano { get; set; };
+        private string tamano;
 
         public abstract string GetNombre();
 
         public string SetTamano(string tam)
         {
             tamano = tam;
+            return tamano;
+        }
+
+        public string GetTamano()
+        {
             return tamano;
         }
 
