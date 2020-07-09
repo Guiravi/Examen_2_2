9109b2cc26bf4642de3d8ae9f3c98431a527a850
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 22:11:16 -0600

    [CrearPizza] Se agregó la clase Masa artesanal que hereda de ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/MasaArtesanal.cs
@@ -0,0 +1,17 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class MasaArtesanal : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Masa Artesanal";
+        }
+
+        public override double GetPrecio()
+        {
+            return 1000;
+        }
+    }
+}
