89cb6921ef60ad3a53b48136cb952d4db947975e
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:46:01 -0600

    [CrearPizza] Se crea la clase concreta Pizza que deriva de Comida

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Pizza.cs
@@ -0,0 +1,12 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Pizza : Comida
+    {
+        public override string GetNombre()
+        {
+            return "Pizza";
+        }
+    }
+}
