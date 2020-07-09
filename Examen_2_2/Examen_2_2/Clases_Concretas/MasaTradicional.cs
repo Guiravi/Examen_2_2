14ed71ff1b3d8283d90b6e06018f5022fffb2fef
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 22:12:09 -0600

    [CrearPizza] Se agregó la clase Masa tradicional que hereda de ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/MasaTradicional.cs
@@ -0,0 +1,17 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class MasaTradicional : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Masa Tradicional";
+        }
+
+        public override double GetPrecio()
+        {
+            return 900;
+        }
+    }
+}
