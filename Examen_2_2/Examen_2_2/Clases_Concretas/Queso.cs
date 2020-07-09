b39ced1d5e3eafd7fb1c54369355f416cdc8d474
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:45:37 -0600

    [CrearPizza] Se crea la clase concreta Queso que deriva de Ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Queso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen_2_2.Clases_Abstractas;
+
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Queso : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Queso";
+        }
+
+        public override double GetPrecio()
+        {
+            return 700;
+        }
+    }
+}
