2a558c789858650da0e8d7ea3a4fa25a74c52085
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:07:40 -0600

    [CrearPizza] Se crea la clase abstracta ingrediente

---
+++ Examen_2_2/Examen_2_2/Code/Ingrediente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen_2_2.Code
+{
+    public abstract class Ingrediente
+    {
+
+    }
+}
