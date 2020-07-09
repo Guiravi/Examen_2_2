cd7d6745881c1c7c0691db909fededfcfcb0258d
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:44:59 -0600

    [CrearPizza] Se crea la clase concreta PastaTomate que deriva de Ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/PastaTomate.cs
@@ -0,0 +1,18 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class PastaTomate : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Pasta de Tomate";
+        }
+
+        public override double GetPrecio()
+        {
+            return 600;
+        }
+    }
+}
