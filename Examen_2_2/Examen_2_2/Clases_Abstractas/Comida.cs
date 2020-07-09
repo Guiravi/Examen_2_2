28261cab79eadee3e8c05d785a31cd94cf3ef76b
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 21:05:17 -0600

    [CrearPizza] Se agrega método para cambiar tamano;

--- Examen_2_2/Examen_2_2/Clases_Abstractas/Comida.cs
+++ Examen_2_2/Examen_2_2/Clases_Abstractas/Comida.cs
@@ -5,8 +5,15 @@ namespace Examen_2_2.Clases_Abstractas
     public abstract class Comida
     {
         public List<Ingrediente> Ingredientes;
+        private string tamano { get; set; };
 
         public abstract string GetNombre();
+
+        public string SetTamano(string tam)
+        {
+            tamano = tam;
+            return tamano;
+        }
 
         public double GetPrecio()
         {
