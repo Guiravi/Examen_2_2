4b4e65f79b9a3c822c75c95ba14889fab5632444
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:44:15 -0600

    [CrearPizza] Se crea la clase concreta cebolla que deriva de Ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Cebolla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen_2_2.Clases_Abstractas;
+
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Cebolla : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Cebolla";
+        }
+
+        public override double GetPrecio()
+        {
+            return 100;
+        }
+    }
+}
