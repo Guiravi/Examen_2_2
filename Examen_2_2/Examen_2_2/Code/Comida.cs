6b6b887cbd8ee73615ef5557c464fa20a4e177f4
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:05:52 -0600

    [CrearPizza] Se añade el método GetPrecio a la clase abstracta Comida.

--- Examen_2_2/Examen_2_2/Code/Comida.cs
+++ Examen_2_2/Examen_2_2/Code/Comida.cs
@@ -7,5 +7,11 @@ namespace Examen_2_2.Code
 {
     public abstract class Comida
     {
+        private double Precio;
+
+        public double GetPrecio()
+        {
+            return Precio;
+        }
     }
 }
