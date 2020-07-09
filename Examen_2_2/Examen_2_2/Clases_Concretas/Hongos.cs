660122910619e4388a19da46166542b95e54fee4
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:44:39 -0600

    [CrearPizza] Se crea la clase concreta Hongos que deriva de Ingrediente

---
+++ Examen_2_2/Examen_2_2/Clases_Concretas/Hongos.cs
@@ -0,0 +1,18 @@
+﻿using Examen_2_2.Clases_Abstractas;
+
+
+namespace Examen_2_2.Clases_Concretas
+{
+    public class Hongos : Ingrediente
+    {
+        public override string GetNombre()
+        {
+            return "Hongos";
+        }
+
+        public override double GetPrecio()
+        {
+            return 250;
+        }
+    }
+}
