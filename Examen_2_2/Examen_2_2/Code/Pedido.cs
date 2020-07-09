aae64706ac8f609aa632f69d43a383ba31ec1ecf
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:05:13 -0600

    [CrearPizza] Se añade el método de agregar comida y el de obtener precio en la clase pedido

--- Examen_2_2/Examen_2_2/Code/Pedido.cs
+++ Examen_2_2/Examen_2_2/Code/Pedido.cs
@@ -9,5 +9,25 @@ namespace Examen_2_2.Code
     {
         public List<Comida> ComidaPedido;
 
+        public Pedido()
+        {
+            ComidaPedido = new List<Comida>();
+        }
+
+        public Comida AddComida(Comida comida)
+        {
+            ComidaPedido.Add(comida);
+            return comida;
+        }
+
+        public double GetPrecio()
+        {
+            double precio = 0.0;
+            for (int i = 0; i < ComidaPedido.Count; i++)
+            {
+                precio += ComidaPedido[i].GetPrecio();
+            }
+            return precio;
+        }
     }
 }
