67544ad1d6bcde64540de31186cad5b6d99da19d
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 20:25:29 -0600

    [CrearPizza] Se eliminan los imports que no se están usando

--- Examen_2_2/Examen_2_2/Clases Abstractas/Ingrediente.cs
+++ Examen_2_2/Examen_2_2/Clases Abstractas/Ingrediente.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Examen_2_2.Code
+﻿namespace Examen_2_2.Code
 {
     public abstract class Ingrediente
     {
