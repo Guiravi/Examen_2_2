1a519792cb2f363042cb45d081242faccb6a3047
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-07-08 19:55:40 -0600

    [CrearPizza] Se crea la clase abstracta Comida

---
+++ Examen_2_2/Examen_2_2/Code/Comida.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen_2_2.Code
+{
+    public abstract class Comida
+    {
+    }
+}
